12c7ca880c8be47b53dbe6b24d1dbda66bb9932d
Tran Ngoc Quoc <tnquoc@umc.local>
2020-04-25 08:48:50 +0700

    initial commit

---
+++ Backup/SamplePDFViewer/Form1.vb
@@ -0,0 +1,28 @@
+﻿Public Class Form1
+
+    Private Sub Form1_FormClosing(ByVal sender As Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles Me.FormClosing
+        PdfViewer1.Dispose() 'Necessary to free resources used by the PDF Viewer
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click_1(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        PdfViewer1.SelectFile()
+        TextBox1.Text = PdfViewer1.FileName
+    End Sub
+
+    Private Sub rbXPDF_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles rbXPDF.CheckedChanged
+        If rbXPDF.Checked Then
+            PdfViewer1.UseXPDF = True
+        Else
+            PdfViewer1.UseXPDF = False
+        End If
+        PdfViewer1.FileName = PdfViewer1.FileName
+    End Sub
+
+  Private Sub btOCR_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btOCR.Click
+    MsgBox(PdfViewer1.OCRCurrentPage)
+  End Sub
+End Class
